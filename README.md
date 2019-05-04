ff7714c9861d805daafb62b9ee8d6f3a3b1bd87b
Tony Jin <jsveron23@gmail.com>
2019-05-04 22:23:13 +0800

    Add extraction scripts

---
+++ README.md
@@ -0,0 +1,28 @@
+# extract-pkg &middot; [![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](LICENSE.md)
+
+Extract package from iOS simulator or Android emulator. Before this this command, you must check 2 things.
+
+- Simulator or emulator should be running
+- The package file(.app or .apk) should be already installed
+
+## Installation & Start
+
+```bash
+$ npm install extract-pkg -g
+```
+
+# Usage
+
+```bash
+$ extract-pkg ios --id=com.some.pkg --to=path/to
+```
+
+If did not use `--rename` or `-n`, the package file name will same as original file name.
+
+```bash
+$ extract-pkg android --id=com.some.pkg --to=path/to --rename=app-debug.apk
+```
+
+## License
+
+MIT
--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,57 @@
 #!/usr/bin/env node
 
-const hello = 'may the force be with you!'
+// TODO:
+// - Support Windows OS for Android
+// - Get device name automatically
 
-console.log(hello)
+const yargs = require('yargs')
+const { log, ios, android } = require('../lib/extract-pkg')
+
+const isWin = process.platform === 'win32'
+
+// no Windows support
+if (isWin) {
+  log.warn('No support Windows platform yet.')
+
+  process.exit(1)
+}
+
+// prettier-ignore
+const argv = yargs
+  .usage('Usage: $0 <ios|android> [options]')
+  .command('ios', 'extract .app from iOS simulator')
+  .command('android', 'extract .apk from Android emulator')
+  .help('help').alias('help', 'h')
+  .version('version', 'v1.0.0').alias('version', 'V')
+  .options({
+    id: {
+      alias: 'i',
+      description: 'bundle, application id (e.g com.awesome.www)',
+      requiresArg: true,
+      required: true
+    },
+    to: {
+      alias: 'd',
+      description: 'destination directory',
+      requiresArg: true,
+      required: true
+    },
+    rename: {
+      alias: 'n',
+      description: 'rename file',
+      requiresArg: true,
+      required: false
+    }
+  })
+  .argv
+
+const { _ } = argv
+const [platform] = _
+
+if (platform === 'ios') {
+  ios(argv)
+}
+
+if (platform === 'android') {
+  android(argv)
+}
